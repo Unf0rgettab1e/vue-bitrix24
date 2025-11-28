5797a2579d2122fe93ee7c233114b91a85f75bac
AnthonyDavydchyk <62406462+AnthonyDavydchyk@users.noreply.github.com>
2025-11-29 01:19:41 +0300

    deploy gh-pages

--- src/shared/indexedDB/index.ts
+++
@@ -1,7 +0,0 @@
-import { IndexedDBStorage } from '@/shared/indexedDB/storage'
-
-export const db = new IndexedDBStorage({
-  dbTitle: 'appDB',
-  version: 1,
-  stores: [{ name: 'companies', keyPath: 'ID' }],
-})
